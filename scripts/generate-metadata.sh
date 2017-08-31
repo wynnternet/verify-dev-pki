90df19bc2f86f2327c8744adf24c120f0109cfab
Anshul Sirur <anshul.sirur@digital.cabinet-office.gov.uk>
2017-08-31 12:01:28 +0100

    TT-731: Regenerate metadata sources
    When the certs are regenerated, the corresponding metadata source with
    inline certs has to be regenerated.
    
    Author: @vixus0

--- scripts/generate-metadata.sh
+++ scripts/generate-metadata.sh
@@ -5,15 +5,30 @@ output="$PWD/metadata/output"
 cadir="$PWD/src/main/resources/ca-certificates"
 certdir="$PWD/src/main/resources/dev-keys"
 
+mkdir -p "$sources/dev/idps"
 mkdir -p "$output"
+rm -f "$output"/dev/metadata.xml
+rm -f "$output"/dev/metadata.signed.xml
 
 # generate
-bundle
+bundle >/dev/null
+
+$PWD/scripts/metadata-sources.rb \
+  "$certdir"/hub_signing_primary.crt \
+  "$certdir"/hub_encryption_primary.crt \
+  "$certdir"/stub_idp_signing_primary.crt \
+  "$sources/dev" || exit 1
+
 bundle exec generate_metadata -c "$sources" -e dev -w -o "$output" \
   --hubCA "$cadir"/dev-root-ca.pem.test \
   --hubCA "$cadir"/dev-hub-ca.pem.test \
   --idpCA "$cadir"/dev-root-ca.pem.test \
   --idpCA "$cadir"/dev-idp-ca.pem.test
+
+if test ! -f "$output"/dev/metadata.xml; then
+  echo "$(tput setaf 1)Failed to generate metadata$(tput sgr0)"
+  exit 1
+fi
 
 # sign
 if test -z `which xmlsectool`; then
